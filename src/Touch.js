8de1df7ced82b208017c64bee8cbc7d941ea5e50
rickwebiii <rickweb@microsoft.com>
2017-12-13 07:11:26 -0800

    Only left mouse button should be able to start or end drags (#77)

--- src/Touch.js
+++ src/Touch.js
@@ -23,6 +23,42 @@ function getEventClientOffset (e) {
     }
 }
 
+// Used for MouseEvent.buttons (note the s on the end).
+const MouseButtons = {
+    Left: 1,
+    Right: 2,
+    Center: 4
+}
+
+// Used for e.button (note the lack of an s on the end).
+const MouseButton = {
+    Left: 0,
+    Center: 1,
+    Right: 2
+}
+
+/**
+ * Only touch events and mouse events where the left button is pressed should initiate a drag.
+ * @param {MouseEvent | TouchEvent} e The event
+ */
+function eventShouldStartDrag(e) {
+    // For touch events, button will be undefined. If e.button is defined,
+    // then it should be MouseButton.Left.
+    return e.button === undefined || e.button !== MouseButton.Left;
+}
+
+/**
+ * Only touch events and mouse events where the left mouse button is no longer held should end a drag.
+ * It's possible the user mouse downs with the left mouse button, then mouse down and ups with the right mouse button.
+ * We don't want releasing the right mouse button to end the drag.
+ * @param {MouseEvent | TouchEvent} e The event
+ */
+function eventShouldEndDrag(e) {
+    // Touch events will have buttons be undefined, while mouse events will have e.buttons's left button
+    // bit field unset if the left mouse button has been released
+    return e.buttons === undefined || (e.buttons & MouseButtons.Left) === 0;
+}
+
 // Polyfill for document.elementsFromPoint
 const elementsFromPoint = ((typeof document !== 'undefined' && document.elementsFromPoint) || function (x,y) {
 
@@ -295,6 +331,10 @@ export class TouchBackend {
     }
 
     handleTopMoveStartCapture (e) {
+        if (!eventShouldStartDrag(e)) {
+            return;
+        }
+
         this.moveStartSourceIds = [];
     }
 
@@ -311,6 +351,10 @@ export class TouchBackend {
     }
 
     handleTopMoveStart (e) {
+        if (!eventShouldStartDrag(e)) {
+            return;
+        }
+
         // Don't prematurely preventDefault() here since it might:
         // 1. Mess up scrolling
         // 2. Mess up long tap (which brings up context menu)
@@ -324,6 +368,10 @@ export class TouchBackend {
     }
 
     handleTopMoveStartDelay (e) {
+        if (!eventShouldStartDrag(e)) {
+            return;
+        }
+
         const delay = (e.type === eventNames.touch.start)
             ? this.delayTouchStart
             : this.delayMouseStart;
@@ -419,6 +467,10 @@ export class TouchBackend {
     }
 
     handleTopMoveEndCapture (e) {
+        if (!eventShouldEndDrag(e)) {
+            return;
+        }
+
         if (!this.monitor.isDragging() || this.monitor.didDrop()) {
             this.moveStartSourceIds = null;
             return;
